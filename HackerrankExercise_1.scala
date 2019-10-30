37185e8da93c95c9f91dc0bd45334777ab998c5d
Isaac Chavez <46462818+isaacchaf@users.noreply.github.com>
2019-10-30 17:07:17 -0600

    Update HackerrankExercise_1.scala

--- HackerrankExercise_1.scala
+++ HackerrankExercise_1.scala
@@ -2,6 +2,26 @@
 // Isaac Emmanuel Chávez Fuentes
 // Alvaro Emilio Rivadeneyra
 
+// Expressions 
+
+/*
+
+  5 year old Shinchan had just started learning Mathematics. Meanwhile, one of his studious classmate, Kazama, 
+  had already written a basic calculator which supports only 3 operations on integral numbers: multiplication , addition , and subtraction . 
+  Since he had just learnt about these operations, he didn't have knowledge of precedence of operators, 
+  and in his calculator all operators had same precedence and left associativity.
+
+  As always Shinchan started to irritate him with his silly question. 
+  He gave Kazama a list of  integers and asked him to insert one of the above operators between 
+  each pair of consecutive integer such that the result obtained after feeding the resulting expression in 
+  Kazama's calculator is divisible by 101. At core Shinchan is a good guy, 
+  so he gave only that list of integers for which the answer always exists.
+
+  Can you help Kazama in creating the required expression? If multiple solutions exists, print any one of them.
+
+
+*/
+
 // Create object
 object Solution {
   //Main method
